dd4b16b029bb13a15fc413f76932d3d8438264a0
sahil102103 <96765261+sahil102103@users.noreply.github.com>
2023-06-16 21:37:47 +0000

    Added items to my portfolio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -60,6 +60,12 @@ const projectList = [
       "A fun side project to create a punch activated flamethrower using an Arduino, accelerometer, and an Arc Lighter. Self-taught mechanical and electrical engineering concepts for its creation",
     url: "",
   },
+  {
+    title: "Java Maze Game (BFS/ DFS. Player controls)",
+    description:
+      "The Java Maze Game challenges players with intricate mazes, while also showcasing the workings of Breadth-First Search (BFS) and Depth-First Search (DFS) algorithms. The mazes are uniquely generated using Kruskal's algorithm from a grid of cells.",
+    url: "https://github.com/sahil102103/Maze",
+  },
 ];
 
 const Portfolio = () => {
